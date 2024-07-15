a34fa2e8bd0b93bbfb2dc191d81781f7ef0e6932
william <willwalkeruk@gmail.com>
2024-07-15 18:31:31 +0100

    hogs development additions

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     // let img: DynamicImage = image::open("src/fun.jpg").expect("Failed to open image.");
     
     let img_gray = img.to_luma8();
-    // let sobel_img = sobel(img_gray);
-    let hogs_img = hogs(img_gray);
-    hogs_img.save("src/output.png").expect("Failed to save image.");
+    let sobel_img = sobel(img_gray);
+    // let hogs_img = hogs(img_gray);
+    sobel_img.save("src/output.png").expect("Failed to save image.");
 }
 
 fn sobel(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8>>{
@@ -45,6 +45,7 @@ fn sobel(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8
 }
 
 fn hogs(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8>>{
+    // thank you https://builtin.com/articles/histogram-of-oriented-gradients
     let height = input.height();
     let width = input.width();
     // let mut buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::new(width, height);
@@ -63,10 +64,43 @@ fn hogs(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8>
         }
     }
     
-    let g_mags : Vec<u8> = gx_buffer.iter().zip(gy_buffer.iter())
-    .map(|(gx, gy)| (((*gx as i32).pow(2) + (*gy as i32).pow(2)) as f64).sqrt() as u8) // could i x -> f64.. perform ops.. --> u8
-    .collect();
-    let gxy_buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, g_mags).expect("Failed to create image buffer.");
+    // let g_mags : Vec<u8> = gx_buffer.iter().zip(gy_buffer.iter())
+    // .map(|(gx, gy)| (((*gx as i32).pow(2) + (*gy as i32).pow(2)) as f64).sqrt() as u8) // could i x -> f64.. perform ops.. --> u8
+    // .collect();
+    
+    let mut angles : Vec<f64> = Vec::new();
+    let mut mags : Vec<u8> = Vec::new();
 
+    let _ : Vec<_> = gx_buffer.iter().zip(gy_buffer.iter())
+    .map(|(gx, gy)| {
+        angles.push(((*gy as f64) / (*gx as f64)).atan());
+        // mags.push((((*gx as i32).pow(2) + (*gy as i32)) .pow(2)).sqrt() as u8);
+        mags.push((((*gx as i32).pow(2) + (*gy as i32).pow(2)) as f64).sqrt() as u8);
+    }) // could i x -> f64.. perform ops.. --> u8
+    .collect();
+    // calculate the normalised histogram values of each cell of the image 
+
+    for y in 0.. (height-1) / 8 {
+        for x in 0.. (width-1) / 8 {
+            let mut hist : [f64; 9] = [0.0; 9];
+
+            for i in 0..8 {
+                for j in 0..8 {
+                    let index = (((y * 8 + i) * width) + (x * 8) + j) as usize;
+                    let magnitude = mags[index];
+                    let angle = angles[index];
+                    let bin = (angle / 20.0).floor() as usize;
+                    
+                    let x = angle % 20 as f64;
+                    hist[bin] += x * magnitude as f64;
+                    hist[bin] += (20.0 - x) * magnitude as f64;
+                    // need to add to the histogram for where mag > bin * 20 deg
+                    // ahem j + 1 according to the article
+                }
+            }
+        }
+    }
+
+    let gxy_buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, mags).expect("Failed to create image buffer.");
     gxy_buffer
 }

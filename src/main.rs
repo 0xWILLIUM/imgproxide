5308c046c6ebe6e976d5988d81130e4fea0be494
william <willwalkeruk@gmail.com>
2024-07-15 19:29:38 +0100

    fixed up some of the hogs iterators and cleaning

--- src/main.rs
+++ src/main.rs
@@ -44,46 +44,52 @@ fn sobel(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8
     buffer
 }
 
-fn hogs(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8>>{
+
+fn hogs(input : ImageBuffer<Luma<u8>, Vec<u8>>) {
+    let hists = hogs_calc_hists(input);
+
+    
+}
+
+fn hogs_calc_hists(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<[f64; 9]>{
     // thank you https://builtin.com/articles/histogram-of-oriented-gradients
     let height = input.height();
     let width = input.width();
     // let mut buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::new(width, height);
-    let mut gy_buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::new(width, height);
-    let mut gx_buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::new(width, height);
+    let mut gy_buffer : ImageBuffer<Luma<i16>, Vec<i16>> = ImageBuffer::new(width, height);
+    let mut gx_buffer : ImageBuffer<Luma<i16>, Vec<i16>> = ImageBuffer::new(width, height);
+    
     for y in 1..height-1 {
         for x in 1..width-1 {
             let val0 = input.get_pixel(x-1, y).0[0] as i32; // [x-1, y]
             let val2 = input.get_pixel(x+1, y).0[0] as i32; // [x+1, y]
-            gx_buffer.put_pixel(x, y, Luma([(-val0 + val2) as u8]));
+            gx_buffer.put_pixel(x, y, Luma([(-val0 + val2) as i16]));
             
             let val0 = input.get_pixel(x, y-1).0[0] as i32; // [x-1, y]
             let val2 = input.get_pixel(x, y+1).0[0] as i32; // [x+1, y]
-            gy_buffer.put_pixel(x, y, Luma([(-val0 + val2) as u8]));
+            gy_buffer.put_pixel(x, y, Luma([(-val0 + val2) as i16]));
 
         }
     }
     
-    // let g_mags : Vec<u8> = gx_buffer.iter().zip(gy_buffer.iter())
-    // .map(|(gx, gy)| (((*gx as i32).pow(2) + (*gy as i32).pow(2)) as f64).sqrt() as u8) // could i x -> f64.. perform ops.. --> u8
-    // .collect();
-    
     let mut angles : Vec<f64> = Vec::new();
-    let mut mags : Vec<u8> = Vec::new();
+    let mut mags : Vec<f64> = Vec::new();
 
-    let _ : Vec<_> = gx_buffer.iter().zip(gy_buffer.iter())
-    .map(|(gx, gy)| {
-        angles.push(((*gy as f64) / (*gx as f64)).atan());
-        // mags.push((((*gx as i32).pow(2) + (*gy as i32)) .pow(2)).sqrt() as u8);
-        mags.push((((*gx as i32).pow(2) + (*gy as i32).pow(2)) as f64).sqrt() as u8);
-    }) // could i x -> f64.. perform ops.. --> u8
-    .collect();
+    for (gx, gy) in gx_buffer.iter().zip(gy_buffer.iter()) {
+        let gx = *gx as f64;
+        let gy = *gy as f64;
+        angles.push(gy.atan2(gx).to_degrees());
+        mags.push((gx * gx + gy * gy).sqrt());
+    }
+
     // calculate the normalised histogram values of each cell of the image 
-
+    let mut normed_hists : Vec<[f64; 9]> = Vec::new();
+    
     for y in 0.. (height-1) / 8 {
         for x in 0.. (width-1) / 8 {
-            let mut hist : [f64; 9] = [0.0; 9];
 
+            let mut hist : [f64; 9] = [0.0; 9];
+            // can this be done through the use of a zip with mag + dir?
             for i in 0..8 {
                 for j in 0..8 {
                     let index = (((y * 8 + i) * width) + (x * 8) + j) as usize;
@@ -91,16 +97,24 @@ fn hogs(input : ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>, Vec<u8>
                     let angle = angles[index];
                     let bin = (angle / 20.0).floor() as usize;
                     
-                    let x = angle % 20 as f64;
-                    hist[bin] += x * magnitude as f64;
-                    hist[bin] += (20.0 - x) * magnitude as f64;
-                    // need to add to the histogram for where mag > bin * 20 deg
-                    // ahem j + 1 according to the article
+                    let share = angle % 20.0 / 20.0;
+                    hist[bin] += (1.0 - share) * magnitude;
+                    hist[(bin+1) % 9] += share * magnitude;
+
                 }
             }
+
+            let sum : f64 = hist.iter().sum();
+            if sum > 0.0 {
+                // normalise!
+                for val in hist.iter_mut() {
+                    *val /= sum;
+                }
+            }
+
+            normed_hists[(y * (width / 8) + x) as usize] = hist;
         }
     }
 
-    let gxy_buffer : ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, mags).expect("Failed to create image buffer.");
-    gxy_buffer
+    normed_hists
 }
